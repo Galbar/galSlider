77a39cc6a5187308851f3340a3314890778e401c
Alessio Linares <alessio@alessio.rocks>
2014-11-15 13:28:10 +0100

    galSliderTools not needed anymore

--- main.js
+++ main.js
@@ -4,79 +4,6 @@
 ////////////////////////////////////////////////////////////////
 
 /**
- * @brief Some utilities
- */
-var galSliderTools = {
-	jQueryMinimumVersion: {
-		p : 1,
-		s : 10,
-		t : 2
-	},
-	onMouseOver: function () {
-		this.galSlider.is_hovered = true;
-	},
-	onMouseOut: function () {
-		this.galSlider.is_hovered = false;
-	},
-	reflow: function (elt) {
-		elt.offsetHeight;
-	},
-	checkjQueryVersion: function (v) {
-		var s = parseInt(v.substring(0,v.indexOf(".")));
-		if (s < this.jQueryMinimumVersion.p)
-			return false;
-		v = v.substring(v.indexOf(".")+1,v.length);
-		s = parseInt(v.substring(0,v.indexOf(".")));
-		if (s < this.jQueryMinimumVersion.s)
-			return false;
-		v = v.substring(v.indexOf(".")+1,v.length);
-		s = parseInt(v);
-		if (s < this.jQueryMinimumVersion.t)
-			return false;
-		return true
-	},
-	checkIfNumber: function(n) {
-		if (typeof n !== "number" || typeof n === "string") {
-			throw new TypeError("(galSlider) Not a number");
-			return false;
-		}
-		return true;
-	},
-	checkIfInRange: function(i, s, f) {
-		if (i < s || i >= f) {
-			throw new RangeError("(galSlider) Number \""+i+"\" not in valid range. Expected number in ["+s+", "+(f-1)+").");
-			return false;
-		}
-		return true;
-	},
-	checkIfValidTransition: function(v, s) {
-		if (s.transition_values.indexOf(v) > -1)
-			return true;
-		else {
-			l = "";
-			var first = true;
-			for (var i = 0; i < s.transition_values.length; i++) {
-				if (first)
-					first = false;
-				else
-					l += ", ";
-				l += "\""+s.transition_values[i]+"\"";
-			}
-			throw new Error("(galSlider) Invalid transition value \""+v+"\". Expected ["+l+"].");
-			return false;
-		}
-	},
-	checkIfHTMLElement: function(e) {
-		if (typeof HTMLElement === "object" ? e instanceof HTMLElement : //DOM2
-			e && typeof e === "object" && e !== null && e.nodeType === 1 && typeof e.nodeName==="string")
-			return true;
-
-		throw new Error("(galSlider) Expected HTMLElement, found:\n"+JSON.stringify(e));
-		return false;
-	}
-};
-
-/**
  * @brief galSlider class constructor
  * @param elem  HTML element to build galSlider
  */
@@ -95,8 +22,8 @@ function galSlider (elem) {
 	if (elem.galSlider != undefined)
 		elem.galSlider.destroy();
 	elem.galSlider = this;
-	elem.addEventListener("mouseover", galSliderTools.onMouseOver);
-	elem.addEventListener("mouseout", galSliderTools.onMouseOut);
+	elem.addEventListener("mouseover", galSlider.onMouseOver);
+	elem.addEventListener("mouseout", galSlider.onMouseOut);
 
 
 	this.self.classList.add("galSlider");
@@ -137,6 +64,91 @@ function galSlider (elem) {
 	if (this.autoplay)
 		this.start();
 };
+
+/**
+ * @brief Some utilities
+ */
+galSlider.jQueryMinimumVersion = {
+	p : 1,
+	s : 0,
+	t : 0,
+	toString: function () {
+		return "" + this.p + "." + this.s + "." + this.t;
+	}
+}
+
+galSlider.onMouseOver = function () {
+	this.galSlider.is_hovered = true;
+}
+
+galSlider.onMouseOut = function () {
+	this.galSlider.is_hovered = false;
+}
+
+galSlider.reflow = function (elt) {
+	elt.offsetHeight;
+}
+
+galSlider.checkjQueryVersion = function (v) {
+	var s = parseInt(v.substring(0,v.indexOf(".")));
+	var ret = true;
+	if (s < this.jQueryMinimumVersion.p) 
+		ret = false;
+	v = v.substring(v.indexOf(".")+1,v.length);
+	s = parseInt(v.substring(0,v.indexOf(".")));
+	if (s < this.jQueryMinimumVersion.s) 
+		ret = false;
+	v = v.substring(v.indexOf(".")+1,v.length);
+	s = parseInt(v);
+	if (s < this.jQueryMinimumVersion.t) 
+		ret = false;
+	if (!ret)
+		throw new Error("(galSlider) Uncompatible jQuery version, not binding jQuery API. Minimum version required: "
+			+ galSlider.jQueryMinimumVersion.toString() + ".");
+}
+
+galSlider.checkIfNumber = function(n) {
+	if (typeof n !== "number" || typeof n === "string") {
+		throw new TypeError("(galSlider) Not a number");
+		return false;
+	}
+	return true;
+}
+
+galSlider.checkIfInRange = function(i, s, f) {
+	if (i < s || i >= f) {
+		throw new RangeError("(galSlider) Number \""+i+"\" not in valid range. Expected number in ["+s+", "+(f-1)+").");
+		return false;
+	}
+	return true;
+}
+
+galSlider.checkIfValidTransition = function(v, s) {
+	if (s.transition_values.indexOf(v) > -1)
+		return true;
+	else {
+		l = "";
+		var first = true;
+		for (var i = 0; i < s.transition_values.length; i++) {
+			if (first)
+				first = false;
+			else
+				l += ", ";
+			l += "\""+s.transition_values[i]+"\"";
+		}
+		throw new Error("(galSlider) Invalid transition value \""+v+"\". Expected ["+l+"].");
+		return false;
+	}
+}
+
+galSlider.checkIfHTMLElement = function(e) {
+	if (typeof HTMLElement === "object" ? e instanceof HTMLElement : //DOM2
+		e && typeof e === "object" && e !== null && e.nodeType === 1 && typeof e.nodeName==="string")
+		return true;
+
+	throw new TypeError("(galSlider) Expected HTMLElement, found:\n"+JSON.stringify(e));
+	return false;
+}
 
 /**
  * @brief Destroy the galSlider instance
@@ -240,7 +252,7 @@ galSlider.prototype.animate = function(prev, curr, next, transition) {
 		curr.style.opacity = "1";
 		prev.style.opacity = "0";
 		next.style.opacity = "0";
-		galSliderTools.reflow(curr);
+		galSlider.reflow(curr);
 		prev.classList.remove("no-transition");
 		curr.classList.remove("no-transition");
 		next.classList.remove("no-transition");
@@ -252,7 +264,7 @@ galSlider.prototype.animate = function(prev, curr, next, transition) {
 		prev.style.left = this.self.style.width;
 		curr.style.left = "0px";
 		next.style.left = '-' + this.self.style.width;
-		galSliderTools.reflow(curr);
+		galSlider.reflow(curr);
 		prev.classList.remove("no-transition");
 		curr.classList.remove("no-transition");
 		next.classList.remove("no-transition");
@@ -263,7 +275,7 @@ galSlider.prototype.animate = function(prev, curr, next, transition) {
 		prev.style.left = '-' + this.self.style.width;
 		curr.style.left = "0px";
 		next.style.left = this.self.style.width;
-		galSliderTools.reflow(curr);
+		galSlider.reflow(curr);
 		prev.classList.remove("no-transition");
 		curr.classList.remove("no-transition");
 		next.classList.remove("no-transition");
@@ -274,7 +286,7 @@ galSlider.prototype.animate = function(prev, curr, next, transition) {
 		prev.style.top = this.self.style.height;
 		curr.style.top = "0px";
 		next.style.top = '-' + this.self.style.height;
-		galSliderTools.reflow(curr);
+		galSlider.reflow(curr);
 		prev.classList.remove("no-transition");
 		curr.classList.remove("no-transition");
 		next.classList.remove("no-transition");
@@ -285,7 +297,7 @@ galSlider.prototype.animate = function(prev, curr, next, transition) {
 		prev.style.top = '-' + this.self.style.height;
 		curr.style.top = "0px";
 		next.style.top = this.self.style.height;
-		galSliderTools.reflow(curr);
+		galSlider.reflow(curr);
 		prev.classList.remove("no-transition");
 		curr.classList.remove("no-transition");
 		next.classList.remove("no-transition");
@@ -337,7 +349,7 @@ galSlider.prototype.getIndex = function(d) {
  * @param  b index of slide to go to
  */
 galSlider.prototype.goTo = function(b) {
-	if (galSliderTools.checkIfNumber(b) && galSliderTools.checkIfInRange(b, 0, this.children.length)) {
+	if (galSlider.checkIfNumber(b) && galSlider.checkIfInRange(b, 0, this.children.length)) {
 		if (this.autoplay) {
 			this.start();
 		}
@@ -355,7 +367,7 @@ galSlider.prototype.goTo = function(b) {
  * @param b name of transition
  */
 galSlider.prototype.setTransition = function (b) {
-	if (galSliderTools.checkIfValidTransition(b, this))
+	if (galSlider.checkIfValidTransition(b, this))
 		this.transition = b;
 };
 
@@ -365,7 +377,7 @@ galSlider.prototype.setTransition = function (b) {
  * @param b time in miliseconds
  */
 galSlider.prototype.setInterval = function (b) {
-	if (galSliderTools.checkIfNumber(b)) {
+	if (galSlider.checkIfNumber(b)) {
 		this.time_interval = b;
 		if (this.autoplay) {
 			this.start();
@@ -400,7 +412,7 @@ galSlider.prototype.stop = function() {
  * @brief insert new slide at the end
  */
 galSlider.prototype.append = function(e) {
-	if (galSliderTools.checkIfHTMLElement(e)) {
+	if (galSlider.checkIfHTMLElement(e)) {
 		if (this.children.length == 0) {
 			e.style.opacity = "1";
 			e.style.zIndex = "1";
@@ -423,9 +435,9 @@ galSlider.prototype.append = function(e) {
  * @brief insert new slide at position indicated
  */
 galSlider.prototype.insertAt = function(e, i) {
-	if (galSliderTools.checkIfNumber(i) &&
-		galSliderTools.checkIfInRange(i, 0, this.children.length) &&
-		galSliderTools.checkIfHTMLElement(e)) {
+	if (galSlider.checkIfNumber(i) &&
+		galSlider.checkIfInRange(i, 0, this.children.length) &&
+		galSlider.checkIfHTMLElement(e)) {
 		if (this.children.length == 0) {
 			e.style.opacity = "1";
 			e.style.zIndex = "1";
@@ -451,8 +463,8 @@ galSlider.prototype.insertAt = function(e, i) {
  *  @return  HTML element being removed
  */
 galSlider.prototype.remove = function(i) {
-	if (galSliderTools.checkIfNumber(i) &&
-		galSliderTools.checkIfInRange(i, 0, this.children.length)) {
+	if (galSlider.checkIfNumber(i) &&
+		galSlider.checkIfInRange(i, 0, this.children.length)) {
 		var ret = this.children[i];
 		if (i == this.getIndex())
 			this.next();
@@ -462,11 +474,11 @@ galSlider.prototype.remove = function(i) {
 	}
 };
 
-if (window.jQuery && galSliderTools.checkjQueryVersion(jQuery.fn.jquery)) {
+if (galSlider.checkjQueryVersion(jQuery.fn.jquery)) {
 	(function(jQuery) {
-		jQuery.fn.galSlider = function(a,b,c) {
-			jQuery.each(jQuery(this), function (slider_index, elem) {
-				var s = elem.galSlider;
+		galSlider = function(a,b,c) {
+			this.each(function () {
+				var s = this.galSlider;
 				if (a == "start") {
 					s.start();
 				}
